c6ddd16efca7abe8a6f7ed128ed88c620bc3ffe2
alberto <ferrandezmirallesalberto@gmail.com>
2023-01-09 17:14:37 +0100

    cambios en el titulo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GiftExpertApp from './GiftExpertApp'
+import GifFun from './GifFun'
 import './styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GiftExpertApp />
+    <GifFun />
   </React.StrictMode>,
 )
